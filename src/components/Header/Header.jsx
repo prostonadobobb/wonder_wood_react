95e80c4fed344ae9c8a0a0eb3cd8562c5cbbcc52
Volodymyr <prostonadobobb@gmail.com>
2024-02-24 01:08:44 +0200

    add burger + nav fos size < 1300

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,51 +3,78 @@ import './Header.scss';
 import logo from './HeaderImg/logo.png'
 import icon_home from './HeaderImg/icon-home.svg'
 import icon_phone from './HeaderImg/icon-phone.svg'
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 
 export const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [openBurger, setOpenBurger] = useState(false);
+  const navRef = useRef(null);
+  const navbarLinkClass = classNames("navbar__link", {"scroll": isScrolled});
+  console.log(isScrolled, 'isScrolled', openBurger, 'openBurger', navRef);
 
-  const navbarLinkClass = classNames("navbar_link", {"scroll": isScrolled});
-
+  const handleScroll = useCallback(() => {
+    const shouldScroll = window.scrollY > 30;
+    if (isScrolled !== shouldScroll) {
+      setIsScrolled(shouldScroll);
+    }
+  }, [isScrolled]);
+  
   useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 30) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
+    window.addEventListener('scroll', handleScroll);
+  
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
     };
-      window.addEventListener('scroll', handleScroll);
-
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      
-    };
+  }, [handleScroll]);
+  
+  const handleClickOutside = useCallback((e) => {
+    if (navRef.current && !navRef.current.contains(e.target)) {
+      setOpenBurger(false);
+    }
   }, []);
+  
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [handleClickOutside]);
+  
 
   return (
     <section className={classNames("header", {"scroll": isScrolled})}>
       <div className="container">
-        <div className="header_container">
-          <NavLink to='/' className="header_logo">
-            <img src={logo} alt="header-logo" className={classNames("header_logo-img", {"scroll": isScrolled})} />
+        <div className="header__container">
+          <NavLink to='/' className="header__logo">
+            <img src={logo} alt="header-logo" className={classNames("header__logo-img", {"scroll": isScrolled})} />
+            <span>WonderWood</span>
           </NavLink>
 
-          <nav className={classNames("navbar", {"scroll": isScrolled})}>
-            <ul className="navbar_list">
+          <button 
+            ref={navRef}
+            className={`header__burger ${openBurger ? 'active' : ''}`}
+            onClick={() => setOpenBurger(!openBurger)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+
+          <nav  className={classNames("navbar", {"scroll": isScrolled, "active": openBurger})}>
+            <div className="navbar__list">
               <NavLink to='/' className={navbarLinkClass}>
-                <img src={icon_home} alt="icon_home" className='navbar_icon' />
+                <img src={icon_home} alt="icon__home" className='navbar__icon' />
               </NavLink>
               <NavLink to='products' className={navbarLinkClass}>стелажі</NavLink>
               <NavLink to='instruction' className={navbarLinkClass}>інструкція</NavLink>
               <NavLink to='cartridges' className={navbarLinkClass}>картриджі</NavLink>
               <NavLink to='contacts' className={navbarLinkClass}>контакти</NavLink>
-            </ul>
-            <div className="navbar_contact">
-              <NavLink to={`tel: ${+380631001010}`} className="navbar_link-phone">
-                <img src={icon_phone} alt="icon-phone" className="contact_icon" />
+            </div>
+            <div className="navbar__contact">
+              <NavLink to={`tel: ${+380631001010}`} className="navbar__link-phone">
+                <img src={icon_phone} alt="icon-phone" className="contact__icon" />
                 +3(063) 100-10-10
               </NavLink>
             </div>
